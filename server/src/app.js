4af3da374e01af677caf920f1f3583b8ccea583f
AMAN-crypto-alt <amankumarbusiness7870@gmail.com>
2026-05-18 03:04:45 +0530

    chatgpt2

--- server/src/app.js
+++ server/src/app.js
@@ -75,7 +75,7 @@ app.use("/api/deep-analysis", deepAnalysisRoutes);
 // 🔥 Serve React Frontend
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
 
